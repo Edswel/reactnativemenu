7dcfba86319331ab361e7c6be555f762a6a57494
Edswel <franckini@yahoo.com>
2022-04-25 09:39:59 +0100

    Initial styling for categories screen

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,10 +1,10 @@
-import { View, Pressable, Text } from 'react-native';
+import { View, Pressable, Text, StyleSheet } from 'react-native';
 
 function CategoryGridTile({ title, color }) {
     return (
-        <View>
-            <Pressable>
-                <View>
+        <View style={styles.gridItem}>
+            <Pressable style={styles.button}>
+                <View style={styles.innerContainer}>
                     <Text>{title}</Text>
                 </View>
             </Pressable>
@@ -13,3 +13,27 @@ function CategoryGridTile({ title, color }) {
 }
 
 export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+    gridItem: {
+        flex: 1,
+        margin: 16,
+        height: 150,
+        borderRadius: 8,
+        elevation: 4,
+        backgroundColor: '#fff',
+        shadowColor: '#000',
+        shadowOpacity: 0.25,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 8
+    },
+    button: {
+        flex: 1
+    },
+    innerContainer: {
+        flex: 1,
+        padding: 16,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
