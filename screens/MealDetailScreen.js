1c7270b22f1a4005e05a2adea4979b6fb6c57d1a
Edswel <franckini@yahoo.com>
2022-04-27 11:00:43 +0100

    Style the meal detail screen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,4 +1,5 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+import List from '../components/List';
 import MealDetails from '../components/MealDetails';
 import { MEALS } from '../data/dummy-data';
 
@@ -7,18 +8,58 @@ function MealDetailScreen({ route }) {
 
     const selectedMeal = MEALS.find((meal) => meal.id === mealId);
     return (
-        <View>
-            <Image source={{ uri: selectedMeal.imageUrl }} />
-            <Text>{selectedMeal.title}</Text>
+        <ScrollView style={styles.rootContainer}>
+            <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+            <Text style={styles.title}>{selectedMeal.title}</Text>
             <MealDetails duration={selectedMeal.duration} complexity={selectedMeal.complexity} affordability={selectedMeal.affordability} />
-            <Text>Ingredients</Text>
-            {selectedMeal.ingredients.map((ingredient) => (<Text key={ingredient}>{ingredient}</Text>))}
-            <Text>Steps</Text>
-            {selectedMeal.steps.map((step) => (
-                <Text key={step}>{step}</Text>
-            ))}
-        </View>
+            <View style={styles.listOuterContainer}>
+                <View style={styles.listContainer}>
+                    <View style={styles.subtitleContainer}>
+                        <Text style={styles.subtitle}>Ingredients</Text>
+                    </View>
+                    <List data={selectedMeal.ingredients} />
+                    <View style={styles.subtitleContainer}>
+                        <Text style={styles.subtitle}>Steps</Text>
+                    </View>
+                    <List data={selectedMeal.steps} />
+                </View>
+            </View>
+        </ScrollView>
     )
 }
 
 export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        marginBottom: 32
+    },
+    image: {
+        width: '100%',
+        height: 350
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        margin: 8,
+        textAlign: 'center'
+    },
+    subtitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    subtitleContainer: {
+        padding: 6,
+        marginHorizontal: 12,
+        marginVertical: 4,
+        borderBottomColor: '#000',
+        borderBottomWidth: 2
+    },
+    listOuterContainer: {
+        alignItems: 'center'
+    },
+    listContainer: {
+        width: '80%'
+    }
+});
